e648b2e70d2c26b1cc3a6af63a3cf90a8c22df3f
lkenzo <lkenzo@pop-os.localdomain>
2023-09-14 21:56:50 -0300

    WIP - adding invalid parameter handling

--- gerador_de_cpf/main.go
+++ gerador_de_cpf/main.go
@@ -1,15 +1,36 @@
 package main
 
 import (
-"github.com/gofiber/fiber/v2"
-"fmt"
+	"github.com/gofiber/fiber/v2"
+	"strconv"
 )
-func main() {
-    app := fiber.New()
 
-    app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Hello, World!")
-    })
-	fmt.Println("yeah")
-    app.Listen(":3000")
+func main() { 
+	app := fiber.New()
+
+	app.Get("/:ammount?", func(c *fiber.Ctx) error {
+		if c.Params("ammount") == "" {
+			return c.SendString("empty ammount")
+		}
+		return c.SendString(getCpf(c.Params("ammount")))
+	})
+
+    	app.Listen(":3000")
 }
+
+func getCpf(ammount string) string {
+
+	response := ""
+
+	parameterInt, err := strconv.Atoi(ammount)
+	if err != nil {
+		return "invalid ammount, use a integer"
+	}
+
+	for i := 1; i <= parameterInt; i++ {
+		response += "a"
+	}
+
+	return response
+}
+
